6d746857a6ae2659307b1625082806851bd95921
tanyavasilkiv21 <tanyavasilkiv21@gmail.com>
2024-04-19 22:12:29 +0300

    test2

---
+++ DroneAndTurrets/Source/DroneAndTurrets/DroneAndTurrets.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DroneAndTurrets.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DroneAndTurrets, "DroneAndTurrets" );
